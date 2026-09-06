79505acf84513e1ba4fddaf5d0c0101d0eb36073
𝖊𝖈𝖍𝖔 <tiny.cod3r@gmail.com>
2026-09-07 02:20:55 +0300

    fix(content): sync settings on storage change

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -2,12 +2,25 @@ import { defineContentScript } from 'wxt/utils/define-content-script';
 import { Settings } from '@/utils/defaults';
 import { onSelect, onSelectEnd, clean } from '@/lib/selection';
 import { getSettings } from '@/lib/settings';
+import { browser } from 'wxt/browser';
 
 export default defineContentScript({
   matches: ['*://*/*'],
 
   async main() {
     let settings: Settings = await getSettings();
+
+    browser.storage.onChanged.addListener((changes, area) => {
+      if (area !== 'local') return;
+
+      for (const key in changes) {
+        if (key === 'collectedText') continue;
+
+        if (changes[key].newValue !== changes[key].oldValue) {
+          settings = { ...settings, [key]: changes[key].newValue };
+        }
+      }
+    });
 
     document.addEventListener('selectionchange', () => {
       if (settings.mode !== 'disabled') {
